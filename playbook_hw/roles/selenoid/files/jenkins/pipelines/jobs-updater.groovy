2f4a3e16db774fd7b17f761193a67fa74c7aae90
ZoyaSergeeva <muzazoya@gmail.com>
2025-08-18 17:22:26 +0300

    fixing job failure

--- playbook_hw/roles/selenoid/files/jenkins/pipelines/jobs-updater.groovy
+++ playbook_hw/roles/selenoid/files/jenkins/pipelines/jobs-updater.groovy
@@ -18,7 +18,6 @@ keep_descriptors=False
 url=http://localhost/jenkins/
 username=${userName}
 password=${userPassword}
-timeout=60
 """
                 echo "конифг файл job.ini"
                 writeFile(file: 'playbook_hw/roles/selenoid/files/jenkins/job.ini', text: iniConfig, encoding: "UTF-8")
@@ -26,9 +25,26 @@ timeout=60
         }
 
         stage("Upload jobs") {
-            dir('playbook_hw/roles/selenoid/files/jenkins/') {
-                sh "jenkins-jobs --conf ./job.ini update ./jobs/"
-                }
+            echo "обновление джобов"
+            dir('playbook_hw/roles/selenoid/files/jenkins') {
+                sh '''
+                    echo "=== Проверяем что установлено ==="
+                    which python3 || echo "python3 not found"
+                    which jenkins-jobs || echo "jenkins-jobs not found"
+                    python3 -c "import jenkins_jobs" && echo "jenkins_jobs module found" || echo "jenkins_jobs module not found"
+                    find /usr -name "*jenkins-jobs*" 2>/dev/null || echo "jenkins-jobs executable not found"
+                    
+                    echo "=== Пробуем разные варианты запуска ==="
+                    python3 -m jenkins_jobs --conf ./job.ini update ./jobs/ || echo "python3 -m jenkins_jobs failed"
+                '''
             }
+        }
+
+        stage("Verify") {
+            echo "Проверка обновления джобов"
+            dir('playbook_hw/roles/selenoid/files/jenkins') {
+                sh "jenkins-jobs --conf ./job.ini list"
+            }
+        }
     }
 }
